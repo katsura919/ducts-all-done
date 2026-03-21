bfdf323e62aa92a091134f6be31b0c5594c1f4df
katsura919 <katsuragik919@gmail.com>
2026-03-21 21:43:19 +0800

    resize hero image

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -84,7 +84,7 @@ export default function Hero() {
           src="/asset/hero-image-tech.png"
           alt="Duct cleaning technician overlay"
           fill
-          className="object-contain object-bottom z-20 origin-bottom md:scale-[1.85] md:translate-x-[12%] lg:scale-[1.45] lg:translate-x-[2%] xl:scale-[1.35] xl:translate-x-0"
+          className="object-contain object-bottom z-20 origin-bottom md:scale-[0.85] md:translate-x-[20%] lg:scale-[0.75] lg:translate-x-[15%] xl:scale-[0.7] xl:translate-x-0"
           priority
           sizes="(max-width: 1024px) 80vw, 60vw"
         />
